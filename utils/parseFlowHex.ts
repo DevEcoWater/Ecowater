ee9db383fcd8ca3156b4a5ac335cbf5bd314b681
Juan Segundo <juansepincha@gmail.com>
2025-03-24 20:30:59 -0300

    feat: :zap: add parse function for each case the meter data need

---
+++ utils/parseFlowHex.ts
@@ -0,0 +1,30 @@
+export const parseFlowHex = (input: string, unit: string): string => {
+  if (input.includes(".")) {
+    return input;
+  }
+
+  const cleanHex = input.replace(/\s+|\W+/g, "");
+  if (cleanHex.length < 6) {
+    throw new Error("Invalid hex string. Expected at least 6 characters.");
+  }
+
+  const unitCode = cleanHex.slice(0, 2);
+  const byte2 = cleanHex.slice(2, 4);
+  const byte3 = cleanHex.slice(4, 6);
+
+  const unitFactors: Record<string, number> = {
+    "2A": 0.0001,
+    "2B": 0.001,
+    "2C": 0.01,
+    "2D": 0.1,
+    "2E": 1,
+  };
+
+  const factor = unitFactors[unitCode.toUpperCase()];
+
+  const flowDecimalString = byte3 + byte2;
+
+  const finalResult = parseInt(flowDecimalString) * factor;
+
+  return finalResult.toFixed(4) + unit;
+};
