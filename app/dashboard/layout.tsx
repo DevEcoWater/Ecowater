f5725dce71ac1c1aef1fdbb45cb31615785272e7
Marcos Mercado <mdmercado6@gmail.com>
2025-04-28 02:11:52 -0300

    feat: wrap dashboard layout in GoogleMapsProvider for enhanced map functionality

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { cn } from "@/lib/utils";
+import { GoogleMapsProvider } from "@/providers/google-maps-provider";
 
 export default function RootLayout({
   children,
@@ -16,24 +17,25 @@ export default function RootLayout({
   return (
     <UiSidebarProvider>
       <ThemeProvider attribute="class" defaultTheme="light">
-        <AppSidebar />
-        <SidebarInset>
-          <div
-            id="content"
-            className={cn(
-              "ml-auto w-full max-w-full",
-              "peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]",
-              "peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]",
-              "transition-[width] duration-200 ease-linear",
-              "flex h-svh flex-col",
-              "group-data-[scroll-locked=1]/body:h-full",
-              "group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh"
-            )}
-          >
-            {/* ===== Content ===== */}
-            {children}
-          </div>
-        </SidebarInset>
+        <GoogleMapsProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <div
+              id="content"
+              className={cn(
+                "ml-auto w-full max-w-full",
+                "peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]",
+                "peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]",
+                "transition-[width] duration-200 ease-linear",
+                "flex h-svh flex-col",
+                "group-data-[scroll-locked=1]/body:h-full",
+                "group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh"
+              )}>
+              {/* ===== Content ===== */}
+              {children}
+            </div>
+          </SidebarInset>
+        </GoogleMapsProvider>
       </ThemeProvider>
     </UiSidebarProvider>
   );
