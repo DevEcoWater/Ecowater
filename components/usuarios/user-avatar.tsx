46923d9e42723ad565a87c3bf422ea8fb43bb9da
Juan Segundo <juansepincha@gmail.com>
2025-09-10 23:29:51 -0300

    feat: :art: remove unused tags and import

--- components/usuarios/user-avatar.tsx
+++ components/usuarios/user-avatar.tsx
@@ -1,6 +1,7 @@
 import type React from "react";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { formatUserType } from "@/utils/formatUserType";
+import { getInitials } from "@/utils/getInitials";
 
 interface UserAvatarProps {
   firstName: string;
@@ -13,7 +14,7 @@ const UserAvatar: React.FC<UserAvatarProps> = ({
   lastName,
   role,
 }) => {
-  const initials = `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
+  const initials = getInitials(firstName, lastName);
 
   return (
     <div className="flex items-center gap-3">
