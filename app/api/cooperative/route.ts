34c41ab5f2f307bdc960058a94720bb2190d026b
Juan Segundo <juansepincha@gmail.com>
2025-10-09 01:00:09 -0300

    feat: :art: add cooperative logo and update schema

--- app/api/cooperative/route.ts
+++ app/api/cooperative/route.ts
@@ -1,23 +1,23 @@
 // app/api/cooperative/route.ts
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const cooperatives = await prisma.cooperative.findFirstOrThrow();
-  return NextResponse.json(cooperatives);
+  const cooperative = await prisma.cooperative.findFirstOrThrow();
+
+  return NextResponse.json(cooperative);
 }
 
 export async function PUT(req: Request) {
   try {
     const data = await req.json();
-    const existing = await prisma.cooperative.findFirst();
-
+    const existing = await prisma.cooperative.findFirstOrThrow();
     const updated = await prisma.cooperative.update({
       where: { id: existing.id },
       data,
     });
-
     return NextResponse.json(updated);
   } catch (error) {
     console.error("[UPDATE COOPERATIVE]", error);
