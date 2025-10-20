6b676317d0f9dfaba022d3a511b490166b45ae69
Juan Segundo <juansepincha@gmail.com>
2025-10-19 21:51:53 -0300

    feat: :art: create video modal and update page header component

--- components/dashboard/page-header/PageHeaderRender.tsx
+++ components/dashboard/page-header/PageHeaderRender.tsx
@@ -12,6 +12,9 @@ export function PageHeaderRenderer() {
     <PageHeader
       title={title}
       description={description}
+      helpVideoUrl="https://firebasestorage.googleapis.com/v0/b/ecowater-dashboard.firebasestorage.app/o/Video%20Demo_EcoWater.mp4?alt=media&token=4ae20fe2-c398-406d-afe3-ae7de0476603"
+      helpVideoTitle="Primeros pasos"
+      helpVideoDescription="Aprende a como usar la plataforma"
       showBackButton={pathname === "/dashboard"}
     />
   );
